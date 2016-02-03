714ee6ca465fbb508c5a4f10e082071da7491c21
Drakoshi <taduksaz@live.com>
2016-02-03 00:16:11 +0000

    Spaces, spaces everywhere.

--- Connect Four/Connect_Four.py
+++ Connect Four/Connect_Four.py
@@ -23,7 +23,7 @@ class Game():
         self.__RedScore = 0
         self.__BlueScore = 0
 
-        self.__gFont = ("Arial",16,"bold")
+        self.__gFont = ("Arial", 16, "bold")
 
         self.__CreateGame()
 
@@ -39,7 +39,7 @@ class Game():
         self.__CreateTopFrame()
 
         # Setup canvas for the game 
-        self.__canvas = Canvas(self.__frame,width = W, height = H, bg ="black")
+        self.__canvas = Canvas(self.__frame, width = W, height = H, bg ="black")
         self.__canvas.pack()
   
         for i in range(int(W / D)):
@@ -47,10 +47,10 @@ class Game():
             for j in range(int(H / D)):
                 x = j * D
             
-                self.__canvas.create_oval(x,y,x+D-1,y+D-1,fill = "white") # -1 because they were/are a bit overlapping
+                self.__canvas.create_oval(x, y, x+D-1, y+D-1, fill = "white") # -1 because they were/are a bit overlapping
                 self.__canvas.pack()
 
-        self.__canvas.bind("<Button-1>",self.__Select) # binds the left mouse button and from here it proceeds on
+        self.__canvas.bind("<Button-1>", self.__Select) # binds the left mouse button and from here it proceeds on
         self.__canvas.pack()
     def __CreateTopFrame(self):
         """Creates top frame with current player turn and players scores"""
@@ -59,13 +59,13 @@ class Game():
         topFrame.pack(fill = BOTH)
 
         self.__lNr = 1 # initial number set 
-        self.__pLabel = Label(topFrame,text = "Player 1",fg = "red", font = self.__gFont)
+        self.__pLabel = Label(topFrame, text = "Player 1", fg = "red", font = self.__gFont)
         self.__pLabel.pack()
 
-        self.__rScore = Label(topFrame,text = str(self.__RedScore), fg = "red", font = self.__gFont)
+        self.__rScore = Label(topFrame, text = str(self.__RedScore), fg = "red", font = self.__gFont)
         self.__rScore.pack(side = LEFT)
 
-        self.__bScore = Label(topFrame,text = str(self.__BlueScore), fg = "blue", font = self.__gFont)
+        self.__bScore = Label(topFrame, text = str(self.__BlueScore), fg = "blue", font = self.__gFont)
         self.__bScore.pack(side = RIGHT)
 
     def __RestartGame(self):
@@ -75,115 +75,115 @@ class Game():
         self.__frame.pack()
         self.__CreateGame()
     
-    def __Select(self,event):
+    def __Select(self, event):
         """Selects and proceeds on with filling the circle"""
 
-        objectID = self.__canvas.find_closest(event.x,event.y)
+        objectID = self.__canvas.find_closest(event.x, event.y)
         self.__CircleFill(objectID[0])    
-    def __CircleFill(self,ID):
+    def __CircleFill(self, ID):
         """Fills the lowest possible circle in the collumn and checks the grid if there is a 4 in a sequence red or blue"""
 
         ID = self.__DropID(ID)
-        cFill = self.__canvas.itemcget(ID,"fill")
+        cFill = self.__canvas.itemcget(ID, "fill")
 
         if self.__lNr == 1 and  cFill == "white":
-            self.__canvas.itemconfig(ID,fill = "red")
+            self.__canvas.itemconfig(ID, fill = "red")
             self.__canvas.update()
             self.__LabelUpdate()
         elif self.__lNr == 2 and cFill == "white":
-            self.__canvas.itemconfig(ID,fill = "blue")
+            self.__canvas.itemconfig(ID, fill = "blue")
             self.__canvas.update()
             self.__LabelUpdate()
         else:
             pass #print("Circle already taken")
 
         self.__Check()
-    def __DropID(self,ID):
+    def __DropID(self, ID):
         """ Actualy increases object ID to make it drop in the grid,
             to always be on lowest possible level to imitate the drop"""
         while True:
-            nextFill = self.__canvas.itemcget(int(ID)+7,"fill")
+            nextFill = self.__canvas.itemcget(int(ID) + 7, "fill")
 
-            if nextFill == "white" and ID+7 <= 49:
+            if nextFill == "white" and ID + 7 <= 49:
                 ID += 7
             else:
                 return ID
     def __LabelUpdate(self):
         """Changes label between Player 1 and 2"""
         if self.__lNr == 1:
-            self.__pLabel.config(text = "Player 2",fg = "blue")
+            self.__pLabel.config(text = "Player 2", fg = "blue")
             self.__pLabel.update()
             self.__lNr = 2
         elif self.__lNr == 2:
-            self.__pLabel.config(text = "Player 1",fg = "red")
+            self.__pLabel.config(text = "Player 1", fg = "red")
             self.__pLabel.update()
             self.__lNr = 1
 
     def __Check(self):
-        for i in range(1,50):
+        for i in range(1, 50):
             row = int(i % 7)
             collumn = int(i / 7)
 
             if row + 3 <= 7:
-                Colours = self.__GetColours(i,1,0) # Right
+                Colours = self.__GetColours(i, 1, 0) # Right
 
-                RedW = self.__Compare("red",Colours)
-                BlueW = self.__Compare("blue",Colours)       
-                if self.__Wins(RedW,BlueW):
+                RedW = self.__Compare("red", Colours)
+                BlueW = self.__Compare("blue", Colours)       
+                if self.__Wins(RedW, BlueW):
                     break
 
             if collumn + 3 <= 7:
-                Colours = self.__GetColours(i,0,1) # Down
+                Colours = self.__GetColours(i, 0, 1) # Down
 
-                RedW = self.__Compare("red",Colours)
-                BlueW = self.__Compare("blue",Colours)
-                if self.__Wins(RedW,BlueW):
+                RedW = self.__Compare("red", Colours)
+                BlueW = self.__Compare("blue", Colours)
+                if self.__Wins(RedW, BlueW):
                     break
 
             if collumn + 3 <= 7 and row + 3 <= 7: # Down Right
-                Colours = self.__GetColours(i,1,1)
+                Colours = self.__GetColours(i, 1, 1)
 
-                RedW = self.__Compare("red",Colours)
-                BlueW = self.__Compare("blue",Colours)
-                if self.__Wins(RedW,BlueW):
+                RedW = self.__Compare("red", Colours)
+                BlueW = self.__Compare("blue", Colours)
+                if self.__Wins(RedW, BlueW):
                     break
 
             if collumn - 3 >= 0 and row - 3 >= 0: # Up Left
-                Colours = self.__GetColours(i,-1,-1)
+                Colours = self.__GetColours(i, -1, -1)
 
-                RedW = self.__Compare("red",Colours)
-                BlueW = self.__Compare("blue",Colours)
-                if self.__Wins(RedW,BlueW):
+                RedW = self.__Compare("red", Colours)
+                BlueW = self.__Compare("blue", Colours)
+                if self.__Wins(RedW, BlueW):
                     break
 
             if collumn + 3 <= 7 and row - 3 >= 0:
-                Colours = self.__GetColours(i,-1,1) # Down Left
+                Colours = self.__GetColours(i, -1, 1) # Down Left
 
-                RedW = self.__Compare("red",Colours)
-                BlueW = self.__Compare("blue",Colours)
-                if self.__Wins(RedW,BlueW):
+                RedW = self.__Compare("red", Colours)
+                BlueW = self.__Compare("blue", Colours)
+                if self.__Wins(RedW, BlueW):
                     break
 
             if collumn - 3 >= 0 and row + 3 <= 7:
-                Colours = self.__GetColours(i,1,-1) # Up Right
+                Colours = self.__GetColours(i, 1, -1) # Up Right
 
-                RedW = self.__Compare("red",Colours)
-                BlueW = self.__Compare("blue",Colours)
-                if self.__Wins(RedW,BlueW):
+                RedW = self.__Compare("red", Colours)
+                BlueW = self.__Compare("blue", Colours)
+                if self.__Wins(RedW, BlueW):
                     break
-    def __GetColours(self,i,AddSign,MulSign):
+    def __GetColours(self, i, AddSign, MulSign):
         """Gets the colours of specific row/collumn,
            AddSign 1 = add, 0 = no add, -1 = minus,
            MulSign 1 = going down, 0 = same collumn, -1 = going up"""
 
         Colours = []
-        Colours.append(self.__canvas.itemcget(i,"fill"))
+        Colours.append(self.__canvas.itemcget(i, "fill"))
         
         for j in range(1,4):
             Colours.append(self.__canvas.itemcget(i + ((j * 7) * MulSign) + (j * AddSign),"fill"))
 
         return Colours
-    def __Compare(self,target,objects):
+    def __Compare(self, target, objects):
         """Compares the colours that were given to it, returns True, if all were True
            Checks for correct types"""
         if type(target) == str and type(objects) == list:
@@ -191,7 +191,7 @@ class Game():
         else:
             raise TypeError("Target must be a string and objects as list")
          
-    def __Wins(self,RedW,BlueW):
+    def __Wins(self, RedW, BlueW):
         """checks if one of the colours won, 
            stops the game, shows buttons"""
 
@@ -209,13 +209,13 @@ class Game():
             WinFrame = Frame(self.__frame)
             WinFrame.pack()
 
-            winLabel = Label(WinFrame,text = (colour + " Won"),font = self.__gFont,fg = colour)
+            winLabel = Label(WinFrame, text = (colour + " Won"), font = self.__gFont, fg = colour)
             winLabel.pack()
 
-            RestartButton = Button(WinFrame,text = "Restart",command = lambda:self.__RestartGame())
+            RestartButton = Button(WinFrame, text = "Restart", command = lambda: self.__RestartGame())
             RestartButton.pack()
 
-            ExitButton = Button(WinFrame,text = "Exit",command = lambda: self.__root.destroy())
+            ExitButton = Button(WinFrame, text = "Exit", command = lambda: self.__root.destroy())
             ExitButton.pack()
 
             return True
